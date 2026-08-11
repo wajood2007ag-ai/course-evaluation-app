fc43788e97001e053c02a3f1641b6d822192558b
wajood2007ag-ai <wajood2007ag@gmail.com>
2026-08-11 20:04:54 +0300

    Add backend/src/main/java/com/courseval/CourseEvalApplication.java

---
+++ backend/src/main/java/com/courseval/CourseEvalApplication.java
@@ -0,0 +1,11 @@
+package com.courseval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseEvalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CourseEvalApplication.class, args);
+    }
+}
